e4f8e91bec0a4e704bc96e6beb1dc24bf49527b4
Adam <adborowski@gmail.com>
2020-08-31 14:05:03 +0200

    Created a simple component and displayed some text as a test

---
+++ goco-login/src/components/login.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+
+// stateless functional component
+const Login = (props) => (
+    
+    <div className="login">
+
+        <p> This is a test. </p>
+
+    </div>
+);
+
+
+export default Login;

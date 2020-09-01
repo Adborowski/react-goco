67583361bed0de98eca4bd1d30e4d9f3f605a5f4
Adam <adborowski@gmail.com>
2020-09-01 14:10:36 +0200

    Added a new router - HomeRouter.js

---
+++ goco-login/src/routers/HomeRouter.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+
+const HomeRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route exact={true} path="/trainings" component={}/>
+                <Route exact={true} path="/games" component={}/>
+                <Route exact={true} path="/social" component={}/>
+                <Route path="/" component={Login}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default HomeRouter;

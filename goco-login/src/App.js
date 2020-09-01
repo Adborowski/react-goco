b52fecd80e4c263f18d85da45fdf5b5426b1baf3
Adam <adborowski@gmail.com>
2020-09-01 10:04:32 +0200

    Imported font: Roboto

--- goco-login/src/App.js
+++ goco-login/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
 
     <div className="App">
-      
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
     <AppRouter></AppRouter>
 
     </div>

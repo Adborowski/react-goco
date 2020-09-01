25a8379df3cad3fad2922d134124ee50271ef4ff
Adam <adborowski@gmail.com>
2020-09-01 11:17:58 +0200

    Added the Styled components API for Material UI and tested in on the login button

--- goco-login/src/components/login.js
+++ goco-login/src/components/login.js
@@ -2,6 +2,17 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
+
+const LoginButton = styled(Button)({
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  });
 
 class Login extends React.Component{
 
@@ -110,7 +121,7 @@ class Login extends React.Component{
                     </Input>
                 </FormControl>
 
-                    <Button m={20} disabled={!this.state.activePassword} className="login-button">Submit</Button>
+                    <LoginButton disabled={!this.state.activePassword} className="login-button">Submit</LoginButton>
 
                     {this.state.usernameError && <p> {this.state.usernameError} </p>}
                     {this.state.passwordError && <p> {this.state.passwordError} </p>}
